e168b2c03e7b54482d37578e76674243df023cd0
Anthony Scinocco <scinocco.a@gmail.com>
2017-08-06 18:36:22 -0400

    html says the form is a login form. But the requirments state it's a sign up form and it has the type of form fields you would use to sign up with.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Normally I'd use VueJS or Angular for my client side app's, especially if reactivity and feeling quick is important.
+ * However, since this just a basic example I don't want to go through the process of setting up an entire build system to compile my vue modules and es6
+ * and give you a bunch of build steps to run.
+ * Instead everything is brought in through CDN's and can run once index.html is opened in a browser.
+ * Minimal overhead was part of my goal here.
+ */
+jQuery(document).ready(function () {
+    alert('pls');
+});
